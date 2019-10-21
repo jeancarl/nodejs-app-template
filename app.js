54cab21014070f0aa1132299a1053fa10f091c81
JeanCarl Bisson <jeancarl@gmail.com>
2019-10-21 14:58:39 -0700

    Added health check endpoint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var os = require('os');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -7,6 +8,13 @@ app.get('/', function (req, res) {
   res.send('Welcome to my app');
 });
 
+app.get('/health', function(req, res) {
+  res.json({"status": "OK"});
+});
+
+app.get('/host', function(req, res) {
+  res.json({"hostname": os.hostname()});
+});
 
 app.listen(PORT)
 console.log('Application Running on port ' + PORT);
